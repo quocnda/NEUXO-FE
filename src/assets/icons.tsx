5c3c61b4f5c250858f8726641dac24726d795108
quocnda <quoca371@gmail.com>
2025-10-14 10:00:00 +0000

    chore: add public assets, icons and SVGs

---
+++ src/assets/icons.tsx
@@ -0,0 +1,305 @@
+import type { LucideIcon } from 'lucide-react';
+import { Camera, ChevronLeft, ChevronRight, Copy, HelpCircle, Loader2, LogOut, User } from 'lucide-react';
+
+import accountManagement from '@/assets/svg/accountManagement.svg';
+import actionIcon from '@/assets/svg/action-icon.svg';
+import add from '@/assets/svg/add.svg';
+import angleDown from '@/assets/svg/angle-down-solid.svg';
+import arrowDown from '@/assets/svg/arrow-down.svg';
+import arrowLeft from '@/assets/svg/arrow-left.svg';
+import assign from '@/assets/svg/assign.svg';
+import attachment from '@/assets/svg/attachment.svg';
+import avatar from '@/assets/svg/avatar.svg';
+import backIcon from '@/assets/svg/back-icon.svg';
+import bell from '@/assets/svg/bell.svg';
+import blacklistCompany from '@/assets/svg/blacklist-company.svg';
+import blueKey from '@/assets/svg/blue-key.svg';
+import bookmark from '@/assets/svg/bookmark.svg';
+import calendar from '@/assets/svg/calendar.svg';
+import calendardays from '@/assets/svg/calendar-days.svg';
+import campaign from '@/assets/svg/campaign.svg';
+import campaignError from '@/assets/svg/campaign-error.svg';
+import campaignRate from '@/assets/svg/campaign-rate.svg';
+import campaignSend from '@/assets/svg/campaign-send.svg';
+import campaignUser from '@/assets/svg/campaign-user.svg';
+import categoryIcon from '@/assets/svg/category-icon.svg';
+import chatgpt from '@/assets/svg/chatgpt.svg';
+import check from '@/assets/svg/check.svg';
+import checkCircle from '@/assets/svg/check-circle.svg';
+import checkFullKey from '@/assets/svg/check-circle-fullkey.svg';
+import checkCircleXs from '@/assets/svg/check-circle-xs.svg';
+import checked from '@/assets/svg/checked.svg';
+import chevronDown from '@/assets/svg/chevron-down.svg';
+import chevronDownCustom from '@/assets/svg/chevron-down-custom.svg';
+import circleBack from '@/assets/svg/circle-back.svg';
+import close from '@/assets/svg/close.svg';
+import closeCircle from '@/assets/svg/close-circle.svg';
+import column from '@/assets/svg/column.svg';
+import company from '@/assets/svg/company.svg';
+import companySidebar from '@/assets/svg/company-sidebar.svg';
+import completeLine from '@/assets/svg/complete-line.svg';
+import contact from '@/assets/svg/contact.svg';
+import copyV2 from '@/assets/svg/copy_v2.svg';
+import copyIcon from '@/assets/svg/copy-icon.svg';
+import countryIcon from '@/assets/svg/country-icon.svg';
+import discord from '@/assets/svg/discord.svg';
+import dollar from '@/assets/svg/dollar.svg';
+import dot from '@/assets/svg/dot.svg';
+import dowloadBill from '@/assets/svg/down-arrow.svg';
+import download from '@/assets/svg/download.svg';
+import drag from '@/assets/svg/drag.svg';
+import edit from '@/assets/svg/edit.svg';
+import ellipsis from '@/assets/svg/ellipsis.svg';
+import empty from '@/assets/svg/empty.svg';
+import error from '@/assets/svg/error.svg';
+import eye from '@/assets/svg/eye.svg';
+import eyeHidden from '@/assets/svg/eye-hidden.svg';
+import facebook from '@/assets/svg/facebook.svg';
+import facebookCircle from '@/assets/svg/facebook_circle.svg';
+import filter from '@/assets/svg/filter.svg';
+import firstKey from '@/assets/svg/first-key.svg';
+import folderOpen from '@/assets/svg/folder-open.svg';
+import followerIcon from '@/assets/svg/follower-icon.svg';
+import bold from '@/assets/svg/font-bold.svg';
+import fullKey from '@/assets/svg/full-key.svg';
+import funding from '@/assets/svg/funding.svg';
+import genideal from '@/assets/svg/genideal.svg';
+import genidealBlue from '@/assets/svg/genidealBlue.svg';
+import google from '@/assets/svg/google.svg';
+import google1 from '@/assets/svg/google1.svg';
+import greenCheck from '@/assets/svg/green-check.svg';
+import greenCheckbox from '@/assets/svg/green-checkbox.svg';
+import headquarterIcon from '@/assets/svg/headquarter-icon.svg';
+import hiring from '@/assets/svg/hiring.svg';
+import hubspot from '@/assets/svg/hubspot.svg';
+import icp from '@/assets/svg/icp.svg';
+import image from '@/assets/svg/image.svg';
+import industryIcon from '@/assets/svg/industry-icon.svg';
+import infor from '@/assets/svg/infor.svg';
+import insecurity from '@/assets/svg/insecurity.svg';
+import itatic from '@/assets/svg/itatic.svg';
+import job from '@/assets/svg/job.svg';
+import job_user from '@/assets/svg/job-user.svg';
+import key from '@/assets/svg/key.svg';
+import labelIcon from '@/assets/svg/label-icon.svg';
+import library from '@/assets/svg/library.svg';
+import light from '@/assets/svg/light.svg';
+import link from '@/assets/svg/link.svg';
+import linkIcon from '@/assets/svg/link-icon.svg';
+import linkedin from '@/assets/svg/linkedin.svg';
+import location from '@/assets/svg/location.svg';
+import lock from '@/assets/svg/lock.svg';
+import logoutRed from '@/assets/svg/logout-red.svg';
+import menu from '@/assets/svg/menu.svg';
+import menuLeft from '@/assets/svg/menuLeft.svg';
+import menuRight from '@/assets/svg/menuRight.svg';
+import message from '@/assets/svg/message.svg';
+import metamask from '@/assets/svg/metamask.svg';
+import moneyBag from '@/assets/svg/money-bag.svg';
+import more from '@/assets/svg/more.svg';
+import newChat from '@/assets/svg/new-chat.svg';
+import news from '@/assets/svg/news.svg';
+import newsIconcom from '@/assets/svg/newsIconcom.svg';
+import notice from '@/assets/svg/notice.svg';
+import noticeGray from '@/assets/svg/notice-gray.svg';
+import office from '@/assets/svg/office.svg';
+import playSolid from '@/assets/svg/play-solid.svg';
+import plus from '@/assets/svg/plus.svg';
+import plusCircle from '@/assets/svg/plus-circle.svg';
+import qrCodeGasStation from '@/assets/svg/qr-code-gas-station.svg';
+import qrCode from '@/assets/svg/qrcode.svg';
+import reload from '@/assets/svg/reload.svg';
+import remove from '@/assets/svg/remove.svg';
+import search from '@/assets/svg/search.svg';
+import searchIcon from '@/assets/svg/search-icon.svg';
+import searchIconCustom from '@/assets/svg/search-icon-custom.svg';
+import secondKey from '@/assets/svg/second-key.svg';
+import security from '@/assets/svg/security.svg';
+import sizeIcon from '@/assets/svg/size-icon.svg';
+import sort from '@/assets/svg/sort.svg';
+import star from '@/assets/svg/star.svg';
+import stars from '@/assets/svg/stars.svg';
+import storage from '@/assets/svg/storage.svg';
+import subdomain from '@/assets/svg/subdomain.svg';
+import greenTick from '@/assets/svg/tick-green.svg';
+import tracking from '@/assets/svg/tracking.svg';
+import trash from '@/assets/svg/trash.svg';
+import trigger from '@/assets/svg/trigger.svg';
+import triggerIcon from '@/assets/svg/trigger-icon.svg';
+import triggerTimeIcon from '@/assets/svg/trigger-time-icon.svg';
+import twitter from '@/assets/svg/twitter.svg';
+import twitterFill from '@/assets/svg/twitter-fill.svg';
+import underline from '@/assets/svg/underline.svg';
+import unlink from '@/assets/svg/unlink.svg';
+import upgade from '@/assets/svg/upgade.svg';
+import upload from '@/assets/svg/upload.svg';
+import userlaptop from '@/assets/svg/user-laptop.svg';
+import warning from '@/assets/svg/warning.svg';
+import watch from '@/assets/svg/watch.svg';
+import watchlist from '@/assets/svg/watch-list.svg';
+import watchlistCompany from '@/assets/svg/watchlist-company.svg';
+import website from '@/assets/svg/website.svg';
+import whatsapp from '@/assets/svg/whatsapp.svg';
+import X from '@/assets/svg/X.svg';
+import XIcon from '@/assets/svg/x-icon.svg';
+import xTwitter from '@/assets/svg/x-twitter.svg';
+import youtube from '@/assets/svg/youtube.svg';
+
+export type Icon = LucideIcon;
+
+const IconList = {
+  newChat,
+  triggerTimeIcon,
+  campaignSend,
+  campaignUser,
+  campaignRate,
+  campaignError,
+  campaign,
+  attachment,
+  followerIcon,
+  dot,
+  library,
+  image,
+  underline,
+  companySidebar,
+  bold,
+  itatic,
+  infor,
+  watch,
+  download,
+  genidealBlue,
+  stars,
+  company,
+  genideal,
+  avatar,
+  industryIcon,
+  contact,
+  headquarterIcon,
+  checked,
+  countryIcon,
+  categoryIcon,
+  labelIcon,
+  triggerIcon,
+  sizeIcon,
+  actionIcon,
+  linkIcon,
+  copyIcon,
+  upgade,
+  drag,
+  remove,
+  website,
+  icp,
+  linkedin,
+  watchlistCompany,
+  xTwitter,
+  google1,
+  blacklistCompany,
+  column,
+  accountManagement,
+  lock,
+  funding,
+  tracking,
+  storage,
+  news,
+  ellipsis,
+  light,
+  job_user,
+  chatgpt,
+  greenCheckbox,
+  newsIconcom,
+  hiring,
+  assign,
+  subdomain,
+  trigger,
+  watchlist,
+  whatsapp,
+  office,
+  logoutRed,
+  add,
+  folderOpen,
+
+  menuRight,
+  empty,
+  hubspot,
+  menuLeft,
+  sort,
+  user: User,
+  userlaptop,
+  job,
+  filter,
+  qrCode,
+  notice,
+  qrCodeGasStation,
+  chevronDownCustom,
+  more,
+  searchIconCustom,
+  noticeGray,
+  XIcon,
+  calendardays,
+  blueKey,
+  completeLine,
+  error,
+  checkFullKey,
+  edit,
+  fullKey,
+  backIcon,
+  check,
+  playSolid,
+  angleDown,
+  warning,
+  eyeHidden,
+  greenCheck,
+  insecurity,
+  arrowLeft,
+  searchIcon,
+  unlink,
+  X,
+  facebookCircle,
+  circleBack,
+  twitter,
+  eye,
+  camera: Camera,
+  link,
+  bookmark,
+  search,
+  closeCircle,
+  reload,
+  checkCircle,
+  secondKey,
+  upload,
+  metamask,
+  trash,
+  plusCircle,
+  security,
+  star,
+  plus,
+  location,
+  moneyBag,
+  message,
+  bell,
+  spinner: Loader2,
+  key,
+  arrowDown,
+  dollar,
+  chevronLeft: ChevronLeft,
+  chevronRight: ChevronRight,
+  copy: Copy,
+  logout: LogOut,
+  helpCircle: HelpCircle,
+  google,
+  facebook,
+  twitterFill,
+  discord,
+  youtube,
+  firstKey,
+  menu,
+  close,
+  calendar,
+  checkCircleXs,
+  greenTick,
+  chevronDown,
+  dowloadBill,
+  copyV2,
+};
+
+export const Icons = IconList as Record<keyof typeof IconList, Icon>;
